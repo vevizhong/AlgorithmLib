45e74176f02a86a0bdafa68e91aedf581379932c
vevizhong <vevizhong@nuaa.edu.cn>
2016-08-26 18:35:01 +0800

    Start from now!

---
+++ README.md
@@ -0,0 +1,8 @@
+# Algorithm-library #
+
+
+##1. Introduction: ##
+　　This is a library & I want to marshal all of my own algorithms or some I learned from others. It will be a good place for me and all of you who can study with me together.
+
+## 2. others  ##
+　　To be continued......
---
+++ Sort Algorithm/Java/selectSort.java
@@ -0,0 +1,23 @@
+public class selectSort {
+
+    public selectSort(){
+       int a[]={1,54,6,3,78,34,12,45};
+       int position=0;
+       for(int i=0;i<a.length;i++){     
+           int j=i+1;
+           position=i;
+           int temp=a[i];
+           for(;j<a.length;j++){
+              if(a[j]<temp){
+                 temp=a[j];
+                 position=j;
+              }
+           }
+           a[position]=a[i];
+           a[i]=temp;
+       }
+
+       for(int i=0;i<a.length;i++)
+           System.out.println(a[i]);
+    }
+}
